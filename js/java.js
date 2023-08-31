626bf411f5ddef7a3b1e2544175c57183d2ae8a2
damac53 <mitchellefetty@gmail.com>
2023-08-31 13:52:15 +0200

    view all product

--- js/java.js
+++ js/java.js
@@ -1,44 +1,9 @@
-const products = [
-    { id: 1, name: "Product A", category: "Category A", price: 10 },
-    { id: 2, name: "Product B", category: "Category B", price: 15 },
-    { id: 3, name: "Product C", category: "Category C", price: 20 },
-   
-    // Add more products
-];
-
-const cart = [];
-
-const productsSection = document.getElementById("products");
-const cartCount = document.getElementById("cart-count");
-
-// Display products on the page
-function displayProducts() {
-    productsSection.innerHTML = "";
-    products.forEach(product => {
-        const card = document.createElement("div");
-        card.classList.add("product-card");
-        card.innerHTML = `
-            <h3>${product.name}</h3>
-            <p>Category: ${product.category}</p>
-            <p>Price: R${product.price}</p>
-            <button onclick="addToCart(${product.id})">Add to Cart</button>
-        `;
-        productsSection.appendChild(card);
+document.addEventListener('DOMContentLoaded', function() {
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Product added to cart!');
+        });
     });
-}
-
-// Add product to the cart
-function addToCart(productId) {
-    const selectedProduct = products.find(product => product.id === productId);
-    cart.push(selectedProduct);
-    updateCartCount();
-}
-
-// Update cart count
-function updateCartCount() {
-    cartCount.textContent = cart.length;
-}
-
-// Initial setup
-displayProducts();
-updateCartCount();
+});
