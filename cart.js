2f25916cc1695007f4c42b0f9369ba614b15a7e3
damac53 <mitchellefetty@gmail.com>
2023-09-06 11:22:35 +0200

    add to cart active

---
+++ cart.js
@@ -0,0 +1,26 @@
+// open cart modal
+const cart = document.querySelector('#cart');
+const cartModalOverlay = document.querySelector('.cart-modal-overlay');
+
+cart.addEventListener('click', () => {
+  if (cartModalOverlay.style.transform === 'translateX(-200%)'){
+    cartModalOverlay.style.transform = 'translateX(0)';
+  } else {
+    cartModalOverlay.style.transform = 'translateX(-200%)';
+  }
+})
+// end of open cart modal
+
+// close cart modal
+const closeBtn = document.querySelector ('#close-btn');
+
+closeBtn.addEventListener('click', () => {
+  cartModalOverlay.style.transform = 'translateX(-200%)';
+});
+
+cartModalOverlay.addEventListener('click', (e) => {
+  if (e.target.classList.contains('cart-modal-overlay')){
+    cartModalOverlay.style.transform = 'translateX(-200%)'
+  }
+})
+// end of close cart modal
--- product.js
+++ product.js
@@ -1,49 +1,26 @@
-// add products to cart
-const addToCart = document.getElementsByClassName('add-to-cart');
-const productRow = document.getElementsByClassName('product-row');
+let cartCount = 0;
 
-for (var i = 0; i < addToCart.length; i++) {
-  cart = addToCart[i];
-  cart.addEventListener('click', addToCartClicked)
-  alert="item added"
+function addToCart(productName) {
+    cartCount++;
+    updateCartCount();
+  
 }
 
-// function addToCartClicked (event) {
-//   button = event.target;
-//   var cartItem = button.parentElement;
-//   var price = cartItem.getElementsByClassName('product-price')[0].innerText;
-  
-//   var imageSrc = cartItem.getElementsByClassName('product-image')[0].src;
-//   addItemToCart (price, imageSrc);
-//   updateCartPrice()
-// }
+function updateCartCount() {
+    const cartCountElement = document.getElementById('cart-count');
+    cartCountElement.textContent = cartCount;
+}
 
-// function addItemToCart (price, imageSrc) {
-//   var productRow = document.createElement('div');
-//   productRow.classList.add('product-row');
-//   var productRows = document.getElementsByClassName('product-rows')[0];
-//   var cartImage = document.getElementsByClassName('cart-image');
-  
-//   for (var i = 0; i < cartImage.length; i++){
-//     if (cartImage[i].src == imageSrc){
-//       alert ('This item has already been added to the cart')
-//       return;
-//     }
-//   }
-  
-//   var cartRowItems = `
-//   <div class="product-row">
-//         <img class="cart-image" src="${imageSrc}" alt="">
-//         <span class ="cart-price">${price}</span>
-//         <input class="product-quantity" type="number" value="1">
-//         <button class="remove-btn">Remove</button>
-//         </div>
-        
-//       `
-//   productRow.innerHTML = cartRowItems;
-//   productRows.append(productRow);
-//   productRow.getElementsByClassName('remove-btn')[0].addEventListener('click', removeItem)
-//   productRow.getElementsByClassName('product-quantity')[0].addEventListener('change', changeQuantity)
-//   updateCartPrice()
-// }
-// end of add products to cart
+
+  // Function to refresh the cart display
+  function refreshCart() {
+    const cartContainer = document.getElementById("cart-count");
+    cartContainer.innerHTML = ""; // Clear the current cart content
+
+    // Loop through the cart data and create elements for each item
+    for (const item of cart) {
+        const cartItem = document.createElement("div");
+        cartItem.textContent = `${item.productName} - $${item.price}`;
+        cartContainer.appendChild(cartItem);
+    }
+}
