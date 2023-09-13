68ef2d6b27e4be7d4c5d2e0def54459c3be33160
damac53 <mitchellefetty@gmail.com>
2023-09-13 09:39:04 +0200

    json file working

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
-// Function to display categories and products
-function displayCategoriesAndProducts(data) {
-    const categoriesContainer = document.getElementById('categories');
+// Fetch data from the JSON file
+fetch('products_data.json')
+.then(response => response.json())
+.then(data => {
+    // Process the data and display it
+    const productsList = document.getElementById('products-list');
 
-    data.categories.forEach(category => {
-        const categoryDiv = document.createElement('div');
-        
-        
-        category.products.forEach(product => {
-            const productDiv = document.createElement('div');
-            productDiv.innerHTML = `
-            <img src="${product.image}" alt="${product.name}" width="100">
-                <p>${product.name}</p>
-                <p>Price: $${product.price}</p>
-               
-            `;
-            categoryDiv.appendChild(productDiv);
-        });
-
-        categoriesContainer.appendChild(categoryDiv);
+    data.forEach(product => {
+        const productElement = document.createElement('div');
+        productElement.innerHTML = `
+        <div class="pro-container">
+        <div class="pro">
+          <img src="${product.image}" alt=" width="200"  height ="250">
+          <div class="description">
+            <span>${product.name}</span>
+            <h4>R${product.price.toFixed(2)}</h4>
+           </div>
+           <a href="#" class="add-to-cart fal fa-shopping-cart cart" ></a>
+        </div>
+    </div>
+        `;
+    
+        productsList.appendChild(productElement);
     });
-}
+})
+.catch(error => {
+    console.error('Error fetching data:', error);
+});
 
 // Fetch data from the JSON file
-fetch('./grocery_data') // Replace 'your-data.json' with the path to your JSON file
-    .then(response => response.json())
-    .then(data => {
-        // Call the function to display data
-        displayCategoriesAndProducts(data);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
+fetch('products_data.json')
+.then(response => response.json())
+.then(data => {
+    // Process the data and display it
+    const productsList = document.getElementById('products');
+
+    data.forEach(product => {
+        const productElement = document.createElement('div');
+        productElement.innerHTML = `
+        <div class="pro-container">
+        <div class="pro">
+          <img src="${product.image}" alt=" width="200"  height ="250">
+          <div class="description">
+            <span>${product.name}</span>
+            <h4>R${product.price.toFixed(2)}</h4>
+           </div>
+           <a href="#" class="add-to-cart fal fa-shopping-cart cart" ></a>
+        </div>
+    </div>
+        `;
+    
+        productsList.appendChild(productElement);
     });
+})
+.catch(error => {
+    console.error('Error fetching data:', error);
+});
